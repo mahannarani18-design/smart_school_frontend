e958d0722b0b6373ba10c9568141714d4ba81339
mahannarani18-design <mahan.narani.18@gmail.com>
2025-09-16 01:37:07 +0330

    Feat: Use new environment variable for API URL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,22 +2,23 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // آدرس پایه از .env
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // تغییر در این خط برای استفاده از متغیر جدید
+  baseURL: process.env.REACT_APP_DJANGO_API_URL, 
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // اینترسپتور برای اضافه کردن توکن به هر درخواست
 apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
